c33e0b39a06af37bd951ddc22048d66f9e090160
Mathieu ABOU-HAYDAR <maabou-h@e1r4p14.42.fr>
2022-01-05 18:50:23 +0100

    commit

--- src/commands.c
+++ src/commands.c
@@ -47,12 +47,6 @@ int	_shlaunch(struct s_client *client) {
 	int status;
 	int fd1, fd2, fd3;
 
-	if ((fd1 = dup2(client->fd, 0)) == -1)
-		_servlog(EDUPSHELL, client);
-	if ((fd2 = dup2(client->fd, 1)) == -1)
-		_servlog(EDUPSHELL, client);
-	if ((fd3 = dup2(client->fd, 2)) == -1)
-		_servlog(EDUPSHELL, client);
 	char *const argv[] = {"/bin/sh", NULL};
 	if ((pid = fork()) < 0)
 		_servlog(EFORKSHELL, client);
@@ -60,10 +54,16 @@ int	_shlaunch(struct s_client *client) {
 		execve("/bin/bash", argv, NULL);
 	else
 	{
-		close(client->fd);
-		printf("durex: spawned shell (%d)\n", client->pid);
+		printf("durex: spawned shell (%d)\n", pid);
+		if ((fd1 = dup2(client->fd, 0)) == -1)
+			_servlog(EDUPSHELL, client);
+		if ((fd2 = dup2(client->fd, 1)) == -1)
+			_servlog(EDUPSHELL, client);
+		if ((fd3 = dup2(client->fd, 2)) == -1)
+			_servlog(EDUPSHELL, client);
 		client->status = IN_SHELL;
 		client->pid = pid;
+		close(client->fd);
 	}
 	return (IN_SHELL);
 }
